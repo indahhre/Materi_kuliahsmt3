11c139aa4eb0eb963af1892cc2b1e7430e550709
indahhre <indahhhre@gmail.com>
2024-10-21 11:46:04 +0700

    pertemuan6

---
+++ Pem_web/Pertemuan4/pertemuan6/latihan1.js
@@ -0,0 +1,14 @@
+// Mencetak Hello World
+    console.log("Hello World");
+// Membuat Variabel dengan JavaScript
+var name = "Buddy";
+let firstname = "Jhon";
+const pie = 3.14; //String
+// pie = 3.5; // Number
+let isStudent = true; //Boolean
+let person = {name: 'john', age: 25}; //Object
+let numbers = [1, 2, 3, 4, 5]; //Array
+// Cek Tipe Variabel
+console.log("tipe variabel adalah :", typeof numbers);
+// Mencetak Variabel
+console.log(firstname, pie, isStudent, person, numbers[3]);
---
+++ Pem_web/Pertemuan4/pertemuan6/latihan2.js
@@ -0,0 +1,26 @@
+//aritmetic operations
+let sum = 10 + 5; //addition
+let difference = 10 - 5; //substraction
+let product = 10 * 5; //multiplication
+let quotient = 10 / 5; //division
+let reminder = 10 % 5; //modulo
+let exponent = 10 ** 5; //exponent
+console.log(sum, difference, product, quotient, reminder, exponent);
+
+//comparison operators
+let isEqual= 5 === "5"; //Strict equality
+let isEqual2 = 5 == "5"; //Not Strict
+let isNotEqual = 10 != "10"; //Strict
+let isNotEqual2 = 10 !== "10"; //Not Strict
+let isGreaterThan = 15 > 10;
+console.log(isEqual, isEqual2, isNotEqual, isNotEqual2, isGreaterThan);
+
+//Logical Operators
+let isBothTrue = true && true;
+let isBothTrue2 = true && false;
+let isEitherTrue = true || false;
+let isEitherTrue2 = true || true;
+let isEitherTrue3 = false || false;
+let isNotTrue = !true;
+let isNotfalse = !false;
+console.log(isBothTrue, isBothTrue2, isEitherTrue,isEitherTrue2, isEitherTrue3, isNotTrue, isNotfalse);
